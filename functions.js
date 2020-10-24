819581e9f9412ed75eac5be0bdf768a66234ffbe
My-Tien Nguyen <my-tien@mailbox.org>
2020-10-24 22:50:59 +0200

    Move svg and javascript into separate files for reuse.
    Use svg symbol tag to be able to define viewBox in the svg file.

---
+++ functions.js
@@ -0,0 +1,54 @@
+const ingredients = document.getElementById('Ingredients');
+const ingredientsClone = document.getElementById('IngredientsClone');
+const multiplicators = document.getElementsByClassName('portions');
+const amounts = document.getElementsByClassName('amount');
+var collapsed = true;
+
+function init() {
+	cloneIngredients();
+
+	for (let multiplicator of multiplicators) {
+		multiplicator.addEventListener('click', function(e) {
+			multiplyIngredients(e.target.value);
+			e.stopPropagation();
+		}, true);
+	}
+
+	multiplyIngredients(multiplicators[0].value);
+}
+
+function cloneIngredients() {
+	let ingredientsTable = ingredients.firstElementChild;
+	let ingredientsTableClone = ingredientsTable.cloneNode(true);
+	ingredientsClone.appendChild(ingredientsTableClone);
+}
+
+function multiplyIngredients(factor) {
+	for (let multiplicator of multiplicators) {
+		multiplicator.value = Number(factor);
+	}
+	for (let i = 0; i < amounts.length; ++i) {
+		amounts[i].textContent = Number(amounts[i].getAttribute('base')) * Number(factor);
+	}
+}
+
+document.addEventListener('scroll', function(e) {
+	first_data_row = ingredients.querySelector('tr');
+	let ingredientsVisible = !(window.scrollY > (ingredients.offsetTop + first_data_row.offsetHeight));
+	ingredientsClone.style.visibility = ingredientsVisible? 'hidden' : 'visible';
+	if (!collapsed && ingredientsClone.style.visibility == 'hidden') {
+		let oldTransition = ingredientsClone.transition;
+		ingredientsClone.transition = '0s';
+		toggleSideBar();
+		ingredientsClone.transition = oldTransition;
+	}
+}, true);
+
+function toggleSideBar() {
+	if (collapsed) {
+		ingredientsClone.style.transform = 'translateX(0%)';
+	} else {
+		ingredientsClone.style.transform = 'translateX(-100%) translateX(1.5rem)';
+	}
+	collapsed = !collapsed;
+}
